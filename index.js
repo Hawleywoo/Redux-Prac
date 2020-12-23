1da5ae55e92260fdfe1f9f0a95f2f6511693dd71
Hawleywoo <alhawley21@gmail.com>
2020-12-22 22:15:33 -0700

    Redux

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello world')
+import { createStore } from 'redux'
+
+
+const store = createStore(() => {})
+
+const incrementAction = {type: 'INCREMENT', payload: '' }
+
+store.dispatch(incrementAction)
+
+const count = (state=0, action) => {
+
+    switch (action.type){
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default: 
+            return state
+    }
+}
